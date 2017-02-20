71c16f917faefd5e3c9aaa0b6f56b69383b7907f
Tenma <tobiotenma@hotmail.com>
2017-02-20 12:57:30 -0800

    Add files via upload

---
+++ LAB4QUES5.rb
@@ -0,0 +1,10 @@
+#Alisson Leiva Salazar
+myList=["Molly","William","Alexander","Jerry","Mary"]
+
+counter=0
+
+#x.count to count values inside array
+while myList.count > counter
+	puts myList[counter]
+	counter +=1
+end
